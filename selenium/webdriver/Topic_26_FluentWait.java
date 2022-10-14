54ec08be760fe2cfbd40246065a948d6c49be367
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-10-14 22:56:40 +0700

    update

---
+++ selenium/webdriver/Topic_26_FluentWait.java
@@ -0,0 +1,61 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_26_FluentWait {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+
+	}
+
+	@Test
+	public void TC_01_() {
+
+	}
+
+	@Test
+	public void TC_02_() {
+
+	}
+
+	@Test
+	public void TC_03_() {
+
+	}
+
+	@Test
+	public void TC_04_() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
