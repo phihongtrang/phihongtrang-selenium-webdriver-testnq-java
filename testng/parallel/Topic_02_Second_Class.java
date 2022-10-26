da1caf1dbac34909a56e172df66eb31ae143a9c9
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-10-26 20:58:13 +0700

    update

--- testng/parallel/Topic_02_Second_Class.java
+++ testng/parallel/Topic_02_Second_Class.java
@@ -1,4 +1,4 @@
-package tech;
+package parallel;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,54 +15,42 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-public class Topic_05_Multiple_Brower {
+public class Topic_02_Second_Class {
 	WebDriver driver;
 	By emailTextbox = By.xpath("//*[@id='email']");
 	By passwordTextbox = By.xpath("//*[@id='pass']");
 	By loginButton = By.xpath("//*[@id='send2']");
 	String projectPath = System.getProperty("user.dir");
 	
-	@Parameters("browser")
-	@BeforeClass
-	public void beforeClass(String browerName) {
-		switch (browerName) {
-		case "chorme":
-			System.setProperty("webdriver.chorme.driver", projectPath + "\\browserDrivers\\chormedriver.exe");
-			driver = new ChromeDriver();
-			
-			break;
-		case "firefox":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			
-			break;
-		case "edge":
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
-			
-			break;
 
-		default:
-			System.out.println("Brower name is not valid");
-			break;
-		}
-		
-		
-		
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
-	}
+		}
+	
+	
 	@Test
-	public void TC_01_LoginToSystem() {
+	public void Login_01_() {
 		driver.get("http://live.techpanda.org/index.php/customer/account/login/");
 		driver.findElement(emailTextbox).sendKeys("selenium_11_01@gmail.com");
 		driver.findElement(passwordTextbox).sendKeys("111111");
+		
+	}
+	
+	
+	@Test
+	public void Login_02_() {
+		
 		driver.findElement(loginButton).click();
 		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//p")).getText().contains("selenium_11_01@gmail.com"));
 		driver.findElement(By.xpath("//header[@id='header']//span[text()='Account']")).click();
 		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
 	}
 	
+	
+
 	
 	
 	@AfterClass
