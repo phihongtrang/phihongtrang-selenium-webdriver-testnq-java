ff2d55133566e6476b769298ad8cab98f7c8e8a2
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-10-31 22:13:29 +0700

    update

--- selenium/javaTester/Topic_03_Data_Type_PII.java
+++ selenium/javaTester/Topic_03_Data_Type_PII.java
@@ -2,11 +2,18 @@ package javaTester;
 
 public class Topic_03_Data_Type_PII {
 	static int number;
+	String address = "Ho Chi Minh";
 	
 	public static void main(String[]args) {
 		 int studentNumber = 0;
+		 
 		  System.out.println(number);
+		  
 		  System.out.println(studentNumber);
+		  
+		  Topic_03_Data_Type_PII topic = new Topic_03_Data_Type_PII();
+		  
+		  System.out.println(topic.address);
 	}
 
 }
