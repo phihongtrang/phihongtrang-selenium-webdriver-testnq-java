791cde321cc31a68ca0b6dc9a0264835be8d2fea
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-11-22 22:25:02 +0700

    update

--- selenium/javaTester/Topic_02_System.java
+++ selenium/javaTester/Topic_02_System.java
@@ -4,7 +4,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Topic_02_System {
 	public static void main(String[] args) {
@@ -16,7 +18,7 @@ public class Topic_02_System {
 		System.out.println(osName);
 		
 		@BeforeClass
-		public void  BeforeClass() {
+		public void BeforeClass() {
 			if (osName.contains("Mac OS")) {
 				System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver");
 				
