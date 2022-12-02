4c4eddf4c6bfc7fa0f97e5f4ad3a0d9ee5c6ea63
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-12-02 22:57:41 +0700

    update

--- selenium/webdriver/Topic_11_Button_Radio_Checkbox.java
+++ selenium/webdriver/Topic_11_Button_Radio_Checkbox.java
@@ -19,7 +19,7 @@ public class Topic_11_Button_Radio_Checkbox {
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		
+
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
@@ -28,37 +28,39 @@ public class Topic_11_Button_Radio_Checkbox {
 	public void TC_01_Button() {
 		driver.get("https://www.fahasa.com/customer/account/create");
 		driver.findElement(By.cssSelector("li.popup-login-tab-login")).click();
-		
+
 		By loginButtonBy = By.cssSelector("button.fhs-btn-login");
 		Assert.assertTrue(driver.findElement(loginButtonBy).isDisplayed());
-		
+
 		driver.findElement(By.id("login_username")).sendKeys("TrangPhi@gmail.net");
 		driver.findElement(By.id("login_password")).sendKeys("123456");
 		sleepInSecond(3);
-		
+
 		Assert.assertTrue(driver.findElement(loginButtonBy).isEnabled());
-		
-		String loginButtonBackgroundHexa = Color.fromString(driver.findElement(loginButtonBy).getCssValue("background-color")).asHex().toUpperCase();
+
+		String loginButtonBackgroundHexa = Color
+				.fromString(driver.findElement(loginButtonBy).getCssValue("background-color")).asHex().toUpperCase();
 		Assert.assertEquals(loginButtonBackgroundHexa, "#C92127");
-		
+
 		System.out.println(driver.findElement(loginButtonBy).getCssValue("background-color"));
 		System.out.println(Color.fromString(driver.findElement(loginButtonBy).getCssValue("background-color")));
 		System.out.println(Color.fromString(driver.findElement(loginButtonBy).getCssValue("background-color")).asHex());
-		System.out.println(Color.fromString(driver.findElement(loginButtonBy).getCssValue("background-color")).asHex().toUpperCase());
+		System.out.println(Color.fromString(driver.findElement(loginButtonBy).getCssValue("background-color")).asHex()
+				.toUpperCase());
 	}
 
 	@Test
 	public void TC_02_Default_Radio_Checkbox() {
 		driver.get("https://demos.telerik.com/kendo-ui/checkbox/index");
-		
+
 		driver.findElement(By.cssSelector("input#eq5")).click();
 		sleepInSecond(3);
 		Assert.assertTrue(driver.findElement(By.cssSelector("input#eq5")).isSelected());
-		
+
 		driver.findElement(By.cssSelector("input#eq5")).click();
 		sleepInSecond(3);
 		Assert.assertFalse(driver.findElement(By.cssSelector("input#eq5")).isSelected());
-		
+
 		driver.get("http://demos.telerik.com/kendo-ui/styling/radios");
 		driver.findElement(By.cssSelector("input#engine3")).click();
 		sleepInSecond(3);
@@ -66,30 +68,41 @@ public class Topic_11_Button_Radio_Checkbox {
 		if (!driver.findElement(By.cssSelector("input#engine3")).isSelected()) {
 			driver.findElement(By.cssSelector("input#engine3")).click();
 			sleepInSecond(3);
-			
+
 		}
-		
+
 		Assert.assertTrue(driver.findElement(By.cssSelector("input#engine3")).isSelected());
-		
+
 	}
-	
 
 	@Test
 	public void TC_03_() {
-		
-	}  
-	
+
+	}
+
 	@Test
 	public void TC_04_() {
-		
+
 	}
-	
+
+	public void checkToCheckbox(By by) {
+			if (!driver.findElement(by).isSelected()) {
+				driver.findElement(by).click();
+				
+			}
+	}
+	public void uncheckToCheckbox(By by) {
+		if (driver.findElement(by).isSelected()) {
+			driver.findElement(by).click();
+			
+		}
+	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-	
+
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
